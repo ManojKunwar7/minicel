893f7884b71ed7a6b14d47d20fa04b327c5f90a8
manoj.kunwar@repushti.com <manoj.kunwar@repushti.com>
2024-11-26 00:56:47 +0530

    first commit

---
+++ nobuild.c
@@ -0,0 +1,16 @@
+#define NOBUILD_IMPLEMENTATION
+#include "./nobuild.h"
+#define CFLAGS "-Wall", "-Wextra", "-std=c11", "-pedantic", "-ggdb"
+
+int main(int argc, char **argv){
+	GO_REBUILD_URSELF(argc, argv);
+	CMD("cc", CFLAGS, "-o", "minicel", "src/main.c");
+		if(argc > 1){
+			if(strcmp(argv[1], "run") == 0){
+				CMD("./minicel");
+			} else {
+				PANIC("'%s' IS UNKNOWN SUBCOMMAND!", argv[1]);
+			}
+		}
+	return 0;
+}
