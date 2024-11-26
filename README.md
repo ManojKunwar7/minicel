d4a33eca28767956d0989cacdeff8e9d7f92b717
manoj.kunwar@repushti.com <manoj.kunwar@repushti.com>
2024-11-26 13:36:50 +0530

    updated gitignore

--- README.md
+++ README.md
@@ -4,20 +4,20 @@ The idea is to implement a batch program that can accept a csv file that looks l
 
 
 ```csv
-A				, B
-1				, 2
-3				, 4
-=A1+B1	, =A2+B2
+A				| B
+1				| 2
+3				| 4
+=A1+B1	| =A2+B2
 ```
 
 And outputs:
 
 
 ```csv
-A	, B
-1	, 2
-3	, 4
-3	, 7
+A	| B
+1	| 2
+3	| 4
+3	| 7
 ```
 
 Basically a simple Excel engine without any UI.
--- src/main.c
+++ src/main.c
@@ -1,7 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
+#include <string.h>
 
-int main(){
-	printf("Hello, minicel! \n");
+void usage(FILE *stream){
+	fprintf(stream ,"Usage: ./minicel <input.csv>\n");
+}
+
+char *slurp_file(const char *file_path,size_t size){
+	assert(0 && "not implemented");
+	return 0;
+}
+
+int main(int argc, char **argv){
+
+	if(argc < 2){
+		usage(stderr);
+		fprintf(stderr, "ERROR: input file is not provided\n");
+		exit(1);
+	}
+
+	const char *input_file_path = argv[1];
+	size_t file_size = 0; 
+	const char *content = slurp_file(input_file_path, file_size);
+	if(content == NULL){
+		fprintf(stderr, "ERROR: could not read file %s:%s \n", input_file_path,strerror(errno));
+	}
+	// 28:26
 	return 0;
 }
